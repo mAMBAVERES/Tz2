7aecd7d30e8bf8a252af19b086c3460441f1b791
ВашÐАлександр <veresoki@icloud.com>
2025-08-02 15:40:00 +0300

    Разрешены конфликты после слияния

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(2, 1 + 1);
+    }
+}
